e5f46e4a052d9c1832635191194303e15e698534
Merchant151 <joshntram@gmail.com>
2026-04-10 00:57:58 -0400

    some brainstomring

--- sim.py
+++ sim.py
@@ -3,4 +3,9 @@ prefix = ['united','coeltion'];
 sufix = ['saturn','conglomerate'];
 factions = [];
 
-def generate_faction_name()
+def generate_faction_name(): 
+    return 'United Saturn'
+
+
+# a loop start date end date 
+# log to a history folder 
