0e73378b574052d590c5b348f296724ec7421261
Merchant151 <joshntram@gmail.com>
2026-04-11 23:24:12 -0400

    created a loop that runs code once per day for aprox onehundred years

--- sim.py
+++ sim.py
@@ -11,6 +11,17 @@ def generate_faction_name():
 # a loop start date end date 
 # log to a history folder 
 
-date = 0
-max date = 100
-python convert int to date
+max_date = 100
+start_date = 10413810000 #2300-01-01
+day = 86400
+
+def main():
+    current_day = start_date
+    while(current_day < start_date + max_date * day * 365):
+        print('today is: ',end = '')
+        print(datetime.fromtimestamp(current_day))
+        current_day = current_day + day
+
+
+print('running')
+main()
