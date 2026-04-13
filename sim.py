548cf0a59ef0d99f254377aa421ab532e12834cf
Merchant151 <joshntram@gmail.com>
2026-04-12 20:42:08 -0400

    added planets and moons

--- sim.py
+++ sim.py
@@ -15,6 +15,13 @@ max_date = 100
 start_date = 10413810000 #2300-01-01
 day = 86400
 
+#prefix returns world starting conditions and should allow for a unique world maybe in the future allow seed generation.
+def prefix():
+    world = {}
+    planet = {"saturn" : {"major_moons": ["Titan",'Rhea',"Iapetus","Dione","Tethys"],"minor_moons":["Enceladus","Mimas","Hyperion","Phoebe","Janus","Epimetheus"]}}
+    world["locale"] = planet
+    return world
+
 def main():
     current_day = start_date
     while(current_day < start_date + max_date * day * 365):
@@ -23,5 +30,5 @@ def main():
         current_day = current_day + day
 
 
-print('running')
+world_data = prefix()
 main()
